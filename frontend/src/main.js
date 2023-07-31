55a7a805ba4aa8b8c9cc902d6ccf872ae693ebb5
Elvio14 <andrew.elvio14@gmail.com>
2023-07-30 21:13:52 -0400

    router and index update 1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import Admin from './Admin.vue'
-import Login from './Login.vue'
 import routerAdmin from './router/routerAdmin.js'
-import routerIndex from './router/routerIndex.js'
 
 
 import './assets/main.css'
@@ -13,16 +10,6 @@ import './assets/main.css'
 
 const app = createApp(App)
 app.use(createPinia());
-app.use(routerIndex)
-
-const admin = createApp(Admin)
-admin.use(createPinia())
-admin.use(routerAdmin)
-
-const login = createApp(Login)
-login.use(createPinia())
-login.use(routerAdmin)
+app.use(routerAdmin)
 
 app.mount('#app')
-login.mount('#login')
-admin.mount('#admin')
