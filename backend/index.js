d458329ef06630cfc930ae816e46296bcf202085
Elvio14 <andrew.elvio14@gmail.com>
2023-09-02 00:35:32 -0400

    cors fix 5

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,12 @@ import {v2 as cloudinary} from 'cloudinary'
 export const app = express()
 app.use(express.json());
 dotenv.config();
-app.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', 'https://issho-deploy-465e55c6c2f1.herokuapp.com')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token')
-    next()
-})
+// app.use((req, res, next) =>{
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token')
+//     next()
+// })
 
 app.use(express.urlencoded({extended: true}))
 
