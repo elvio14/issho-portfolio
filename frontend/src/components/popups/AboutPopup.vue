8f384b8fda98892eb463fd90e7c0995ba7cc4dec
Elvio14 <andrew.elvio14@gmail.com>
2023-12-11 13:14:52 -0500

    webp 2

--- frontend/src/components/popups/AboutPopup.vue
+++ frontend/src/components/popups/AboutPopup.vue
@@ -19,7 +19,7 @@ export default {
         <div class="overlay" @click="emitClose"></div>
         <div class="container-in">
         <div class="content">
-            <img src="/martinYukaBrad.jpg" class="banner">
+            <img src="/martinYukaBrad.webp" class="banner">
             
             <div class="text-container">
                 <div class="center-heading">
