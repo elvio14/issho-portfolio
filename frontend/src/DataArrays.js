f7902196e2ea321d6b1cf6d756aadbd56a48687d
Elvio14 <andrew.elvio14@gmail.com>
2024-04-21 14:55:34 -0400

    FAQ update

--- frontend/src/DataArrays.js
+++ frontend/src/DataArrays.js
@@ -11,14 +11,5 @@ export const FAQS = [
         answer: `Cookies should be stored in an airtight container.  They'll last several days in the cookie jar. 
         But if you find yourself in a situation where you have a stockpile of cookies, we recommend freezing your extra cookies. 
         Just reheat them in the oven for 1 minute at 350 F for a tasty treat.`
-    },
-    {
-        question: "WHICH NEIGHBORHOODS DO YOU SHIP TO?",
-        answer: `We are currently delivering in the City of Toronto and its surrounding neighborhoods (Mississauga, Vaughan, Markham, and Pickering).
-        If you live outside the servicing area, please contact us for more information.`
-    },
-    {
-        question: "WHEN CAN I EXPECT MY ORDER TO BE DELIVERED?",
-        answer: `We will be doing curb side deliveries between 12:00pm to 6:00pm.`
     }
 ]
